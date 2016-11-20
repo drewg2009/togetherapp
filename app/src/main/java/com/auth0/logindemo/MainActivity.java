328bf223cb6dc2a60242861a96f126265ce9c3ce
Reveler <rowe.301@osu.edu>
2016-11-20 08:11:35 -0500

    DataService is now a thing

--- app/src/main/java/com/auth0/logindemo/MainActivity.java
+++ app/src/main/java/com/auth0/logindemo/MainActivity.java
@@ -8,20 +8,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-
-import org.json.JSONArray;
-
-import java.util.HashMap;
-import java.util.Map;
 
 //Borrowing some code examples from Arjunu for Recycler
 
@@ -36,17 +24,17 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private CustomAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private String[] mDataset = {"29 degrees", "Seahawks 24 - 27 Bengals",
-            "Flash missing, vanishes in crisis", "Half Life 3 announced"};
+    private DataService invitedDB = new DataService(); // REMOVE INIT
 
     SharedPreferences preferences;
     String currentEventString = "";
-    private int mDataSetTypes[] = {INBOUND, INBOUND, BROADCAST, DONE}; //view types
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        invitedDB.add(new CardObj("hey", "", "", "", "", "", "", "", "", "", "u"));
+        invitedDB.add(new CardObj("hey", "", "", "", "", "", "", "", "", "", " ")); //remove soon
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -54,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(MainActivity.this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 //Adapter is created in the last step
-        mAdapter = new CustomAdapter(mDataset, mDataSetTypes);
+        mAdapter = new CustomAdapter(invitedDB);
         mRecyclerView.setAdapter(mAdapter);
 
         //final TextView logo = (TextView) findViewById(R.id.logo);
@@ -69,103 +57,103 @@ public class MainActivity extends AppCompatActivity {
         int id = jsonObject.get("id").getAsInt();
         Log.i(TAG, "fetched user id: " + id);
 
-        getInvitations(id);
+        //getInvitations(id);
 
     }
 
 
-    private boolean getInvitations(final int user_id) {
-        boolean flag = false;
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://phplaravel-19273-43928-180256.cloudwaysapps.com/post/invitations/all";
+//    private boolean getInvitations(final int user_id) {
+//        boolean flag = false;
+//        // Instantiate the RequestQueue.
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//        String url = "http://phplaravel-19273-43928-180256.cloudwaysapps.com/post/invitations/all";
+//
+//        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String response) {
+//                Log.d(TAG, "Invitation Response: " + response.toString());
+//
+//                if (!response.equals("false")) {
+//                    //parse invitations and create card objects
+//                    try {
+//                        JSONArray arr = new JSONArray(response);
+//                        for (int i = 0; i < arr.length(); i++) {
+//                            String event_id = arr.getJSONObject(i).getString("event_id");
+//                            //stored in global currentEventString variable
+//                            getEvent(Integer.parseInt(event_id));
+//
+//                            Log.i(TAG, currentEventString);
+//
+//                        }
+//                    } catch (Exception ex) {
+//                        ex.printStackTrace();
+//                    }
+//
+//
+//                } else {
+//                    Log.d(TAG, "Invitation fetch failed failed: " + response.toString());
+//                }
+//
+//
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError volleyError) {
+//                System.out.println("volley Error ................." + volleyError.getMessage());
+//            }
+//        }) {
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("user_id", "" + user_id);
+//                return params;
+//            }
+//        };
+//
+//        queue.add(stringRequest);
+//
+//        return flag;
+//
+//    }
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d(TAG, "Invitation Response: " + response.toString());
-
-                if (!response.equals("false")) {
-                    //parse invitations and create card objects
-                    try {
-                        JSONArray arr = new JSONArray(response);
-                        for (int i = 0; i < arr.length(); i++) {
-                            String event_id = arr.getJSONObject(i).getString("event_id");
-                            //stored in global currentEventString variable
-                            getEvent(Integer.parseInt(event_id));
-
-                            Log.i(TAG, currentEventString);
-
-                        }
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-
-
-                } else {
-                    Log.d(TAG, "Invitation fetch failed failed: " + response.toString());
-                }
-
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                System.out.println("volley Error ................." + volleyError.getMessage());
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("user_id", "" + user_id);
-                return params;
-            }
-        };
-
-        queue.add(stringRequest);
-
-        return flag;
-
-    }
-
-    private boolean getEvent(final int event_id) {
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://phplaravel-19273-43928-180256.cloudwaysapps.com/post/events/singleEvent";
-
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d(TAG, "Event Response: " + response.toString());
-
-                if (!response.equals("false")) {
-                    currentEventString = response;
-                } else {
-                    Log.d(TAG, "Event fetch failed : " + response.toString());
-                    currentEventString = "false";
-                }
-
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError) {
-                System.out.println("volley Error ................." + volleyError.getMessage());
-            }
-        }) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("event_id", "" + event_id);
-                return params;
-            }
-        };
-
-        queue.add(stringRequest);
-
-        return false;
-
-    }
+//    private boolean getEvent(final int event_id) {
+//        // Instantiate the RequestQueue.
+//        RequestQueue queue = Volley.newRequestQueue(this);
+//        String url = "http://phplaravel-19273-43928-180256.cloudwaysapps.com/post/events/singleEvent";
+//
+//        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String response) {
+//                Log.d(TAG, "Event Response: " + response.toString());
+//
+//                if (!response.equals("false")) {
+//                    currentEventString = response;
+//                } else {
+//                    Log.d(TAG, "Event fetch failed : " + response.toString());
+//                    currentEventString = "false";
+//                }
+//
+//
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError volleyError) {
+//                System.out.println("volley Error ................." + volleyError.getMessage());
+//            }
+//        }) {
+//            @Override
+//            protected Map<String, String> getParams() throws AuthFailureError {
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("event_id", "" + event_id);
+//                return params;
+//            }
+//        };
+//
+//        queue.add(stringRequest);
+//
+//        return false;
+//
+//    }
 }
 
 
