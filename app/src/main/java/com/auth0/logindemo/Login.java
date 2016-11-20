63ffe081fa6152de6efe17b1b2d25e3377131bca
Alex Moorhead <moorhead.alex@gmail.com>
2016-11-20 08:18:28 -0500

    Commented to flatten road ahead.

--- app/src/main/java/com/auth0/logindemo/Login.java
+++ app/src/main/java/com/auth0/logindemo/Login.java
@@ -38,6 +38,9 @@ public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Intent i=new Intent(this, CreateEvent.class);
+
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         super.onCreate(savedInstanceState);
