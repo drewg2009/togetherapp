78ee437e1c6942a043e9b24b57c99177dd629f20
Drew Gallagher <drewg2009@gmail.com>
2016-11-19 19:22:35 -0500

    created login shell

--- app/src/main/java/com/auth0/logindemo/Login.java
+++ app/src/main/java/com/auth0/logindemo/Login.java
@@ -1,13 +1,51 @@
 package com.auth0.logindemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    Button loginButton;
+    Button signupButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button)findViewById(R.id.loginButton);
+        signupButton = (Button)findViewById(R.id.signupButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        //to signup page
+        final Intent signup=new Intent(this, Signup.class);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(signup);
+            }
+        });
     }
+
+    private boolean login(){
+
+        EditText emailText=(EditText)findViewById(R.id.email);
+        EditText password=(EditText)findViewById(R.id.password);
+
+        
+
+        return false;
+    }
+
 }
--- app/src/main/java/com/auth0/logindemo/Signup.java
+++ app/src/main/java/com/auth0/logindemo/Signup.java
@@ -3,11 +3,11 @@ package com.auth0.logindemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CreateEvent extends AppCompatActivity {
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_event);
+        setContentView(R.layout.activity_signup);
     }
 }
