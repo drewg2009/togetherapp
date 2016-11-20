c09e3b8d1cfbcf5b91633ffdad3db010259363aa
Drew Gallagher <drewg2009@gmail.com>
2016-11-20 00:07:17 -0500

    signup working

--- app/src/main/java/com/auth0/logindemo/Signup.java
+++ app/src/main/java/com/auth0/logindemo/Signup.java
@@ -1,8 +1,11 @@
 package com.auth0.logindemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
@@ -20,11 +23,22 @@ import java.util.Map;
 public class Signup extends AppCompatActivity {
 
     static String TAG = "SIGNUP";
+    Button signupButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signupButton = (Button)findViewById(R.id.signupButton);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+
     }
 
     private boolean signup() {
@@ -44,16 +58,19 @@ public class Signup extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(this);
         String url = "http://phplaravel-19273-43928-180256.cloudwaysapps.com/post/users/signup";
 
+        final Intent i = new Intent(this, MainActivity.class);
+
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                Log.d(TAG, "Login Response: " + response.toString());
+                Log.d(TAG, "Signup Response: " + response.toString());
+                startActivity(i);
 
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError volleyError) {
-                System.out.println("volley Error ................." + volleyError.getMessage());
+                Log.i(TAG,"volley Error ................." + volleyError.getMessage());
             }
         }) {
             @Override
@@ -61,12 +78,15 @@ public class Signup extends AppCompatActivity {
                 Map<String, String> params = new HashMap<String, String>();
                 params.put("email", email);
                 params.put("password", password);
-                params.put("fullName", fullName);
+                params.put("name", fullName);
                 params.put("broadcaster", broadcaster);
+                params.put("calendar_value", "");
+                params.put("calendar_last_updated", "");
                 return params;
             }
         };
 
+        queue.add(stringRequest);
 
         return flag;
     }
