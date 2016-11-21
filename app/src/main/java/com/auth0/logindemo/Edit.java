c8a587a6fd8c9416c39fe4718270678638bf9d2d
Reveler <rowe.301@osu.edu>
2016-11-21 15:45:00 -0500

    Navigation fixed, except for the BuildEvent Activity.

--- app/src/main/java/com/auth0/logindemo/Edit.java
+++ app/src/main/java/com/auth0/logindemo/Edit.java
@@ -9,5 +9,6 @@ public class Edit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        setTitle("Together | My Events");
     }
 }
